efd24be00db0cd567cd25a70c2614dbd91414c46
karen111-star <karenborda111@gmail.com>
2025-09-03 14:32:48 +0000

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // Objeto con la información
   const data = {
     titulo: "Objetos para la aventura",
     descripcion:
       "Girad los fotodiscos de las Poképaradas y los Gimnasios para recibir objetos que os ayudarán en vuestro viaje por Pokémon GO, como Poké Balls, Bayas, objetos de evolución y mucho más.",
     imagenes: {
-      fondo: "/imagen_pk.PNG" // Guarda la imagen que subiste como fondo.jpg en public/
+      fondo: "/poke-imagen.png" // asegúrate que coincida el nombre exactamente
     }
   };
 
@@ -17,11 +16,8 @@ function App() {
       className="container"
       style={{ backgroundImage: `url(${data.imagenes.fondo})` }}
     >
-      <div className="overlay">
-        <h1>{data.titulo}</h1>
-        <p>{data.descripcion}</p>
-
-      </div>
+      <h1>{data.titulo}</h1>
+      <p>{data.descripcion}</p>
     </div>
   );
 }
