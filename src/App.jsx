65591fda89dfe74c9e677940bffceeac8ed7023c
karen111-star <karenborda111@gmail.com>
2025-09-03 14:09:04 +0000

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Objeto con la información
+  const data = {
+    titulo: "Objetos para la aventura",
+    descripcion:
+      "Girad los fotodiscos de las Poképaradas y los Gimnasios para recibir objetos que os ayudarán en vuestro viaje por Pokémon GO, como Poké Balls, Bayas, objetos de evolución y mucho más.",
+    imagenes: {
+      fondo: "/imagen_pk.PNG" // Guarda la imagen que subiste como fondo.jpg en public/
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="container"
+      style={{ backgroundImage: `url(${data.imagenes.fondo})` }}
+    >
+      <div className="overlay">
+        <h1>{data.titulo}</h1>
+        <p>{data.descripcion}</p>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
