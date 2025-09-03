bf57b38bc76669caf5d3160a8af9c5f28d703c8d
karen111-star <karenborda111@gmail.com>
2025-09-03 14:54:04 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  const data = {
-    titulo: "Items for Your Adventure",
-    descripcion:
-      "Spin Photo Discs at PokéStops and Gyms to receive items that will help you on your Pokémon GO journey, including Poké Balls, Berries, Evolution items, and more.",
-    imagenes: {
-      fondo: "/poke-imagen.png" // asegúrate que coincida el nombre exactamente
-    }
-  };
+//objeto 
+const data = {
+  titulo: "Items for Your Adventure",
+  descripcion:
+    "Spin Photo Discs at PokéStops and Gyms to receive items that will help you on your Pokémon GO journey, including Poké Balls, Berries, Evolution items, and more.",
+  imagen: "/poke-imagen.png"
+};
 
+export default function App() {
   return (
     <div
       className="container"
-      style={{ backgroundImage: `url(${data.imagenes.fondo})` }}
+      style={{ backgroundImage: `url(${data.imagen})` }}
     >
       <h1>{data.titulo}</h1>
       <p>{data.descripcion}</p>
     </div>
   );
 }
-
-export default App;
